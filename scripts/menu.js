4d31f82048e2562c4316b747249579203413cb68
rachidelaid <rachidelaid@gmail.com>
2021-12-23 20:48:52 +0100

    resetting the main branch

--- scripts/menu.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('active');
-  mobileMenu.classList.toggle('show');
-});
