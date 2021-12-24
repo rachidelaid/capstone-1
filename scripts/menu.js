58e84ca93a1e318283829f412f9b2e18f0d74d20
rachidelaid <rachidelaid@gmail.com>
2021-12-24 09:04:12 +0100

    cleaning the menu.js file

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,17 +1,13 @@
 const menuBtn = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-function toggle() {
+function toggleMenu() {
   menuBtn.classList.toggle('active');
   mobileMenu.classList.toggle('show');
 }
 
-menuBtn.addEventListener('click', () => {
-  toggle();
-});
+menuBtn.addEventListener('click', toggleMenu);
 
 mobileMenu.querySelectorAll('.links a').forEach((link) => {
-  link.addEventListener('click', () => {
-    toggle();
-  });
+  link.addEventListener('click', toggleMenu);
 });
